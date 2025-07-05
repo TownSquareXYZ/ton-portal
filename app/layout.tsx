27f096d0c3c40e966bb24343315403fc819a9185
sansx <xiathxth@gmail.com>
2025-07-06 02:43:46 +0800

    feat: add TON connect

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { defaultSEO } from "@/lib/seo";
 import { StructuredData, organizationSchema, websiteSchema } from "@/components/structured-data";
+import { TonConnectProvider } from "@/components/providers/tonconnect-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <TonConnectProvider>
+          {children}
+        </TonConnectProvider>
       </body>
     </html>
   );
